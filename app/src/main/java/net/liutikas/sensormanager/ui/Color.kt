0d0b5d2997f6528591e3bacc211bd5c5c87e350e
Aurimas Liutikas <aurimas@liutikas.net>
2020-09-20 19:11:03 -0700

    Clean up theme colors

--- app/src/main/java/net/liutikas/sensormanager/ui/Color.kt
+++ app/src/main/java/net/liutikas/sensormanager/ui/Color.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.graphics.Color
 val purple200 = Color(0xFF008e76)
 val purple500 = Color(0xFF00bfa5)
 val purple700 = Color(0xFF5df2d6)
-val teal200 = Color(0xFF4A5568)
+val teal200 = Color(0xFF62727b)
